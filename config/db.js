950b2affb60f69687113c9a43375b7db425371dc
Mr. Bishnu Prasad Khanal <bkhanal1@my.centennialcollege.ca>
2019-02-08 09:48:35 -0500

    connestion string added

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ module.exports = {
   //this is an object container
   //each node by defaut is inside {}
   //add property (URI is a property of file db.js)
-  URI: "mongodb://localhost/bishnu"
+  //URI: "mongodb://localhost/bishnu"
+
+  URI:
+    "mongodb://bishnu:Bikalpa22@ds018258.mlab.com:18258/comp308-w2019-lesson4b-exercise"
 
   //to publish in mLab
   // "URI":"mongodb://Bikalpa:Bikalpa@22@ds044989.mlab.com:44989/comp308-w2019-lesson4"
